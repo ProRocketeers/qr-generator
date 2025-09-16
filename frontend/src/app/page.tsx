0e27dc919cca2f51cc05c0349a3405aa7a143edc
Patrik <patrikludvik@gmail.com>
2025-09-16 06:25:41 +0200

    simplify mapper

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
 import { SelectTypeForm } from "../components/client/SelectTypeForm";
 import { FormWrapper } from "../components/server/FormWrapper";
-import type { FormsType } from "@/utils/helpers/getFormsMap";
+import type { FORMS_MAP } from "@/utils/helpers/getFormsMap";
 
-export default async function Home({ searchParams }: { searchParams: { type?: FormsType } }) {
+export default async function Home({ searchParams }: { searchParams: { type?: keyof typeof FORMS_MAP } }) {
 	const { type } = await searchParams;
 
 	return (
--- frontend/src/utils/helpers/getFormsMap.ts
+++ frontend/src/utils/helpers/getFormsMap.ts
@@ -6,13 +6,7 @@ export const FORMS_MAP = {
 	email: EmailForm,
 } as const;
 
-export type FormsType = keyof typeof FORMS_MAP;
-
-type Props<T extends keyof typeof FORMS_MAP> = {
-	type?: T;
-};
-
-export const getFormsMap = <T extends keyof typeof FORMS_MAP>({ type }: Props<T>) => {
+export const getFormsMap = <T extends keyof typeof FORMS_MAP>({ type }: { type?: T }) => {
 	if (!type) {
 		return null;
 	}
