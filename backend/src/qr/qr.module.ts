febfea5380640c8004dfee469a321e051e870d2f
Ondra Sýkora <ondrej@sykora.it>
2026-01-12 17:57:44 +0100

    feat: Update QR code management with new entity controller and enhance db:seed script

--- backend/src/qr/qr.module.ts
+++ backend/src/qr/qr.module.ts
@@ -2,14 +2,15 @@ import { Module } from '@nestjs/common'
 import { MikroOrmModule } from '@mikro-orm/nestjs'
 import { QrEntity } from '@backend/qr/qr.entity'
 import { QrStyleService } from '@backend/qr/qrStyle.service'
-import { QrController } from '@backend/qr/qr.controller'
 import { QrStringService } from './qrString.service'
 import { QrEntityService } from './qrEntity.service'
 import { QrCodeService } from './qrCode.service'
+import { QrController } from '@backend/qr/qr.controller'
+import { QrEntityController } from './qrEntity.controller'
 
 @Module({
   imports: [MikroOrmModule.forFeature([QrEntity])],
-  controllers: [QrController],
+  controllers: [QrController, QrEntityController],
   providers: [QrStyleService, QrStringService, QrEntityService, QrCodeService],
 })
 export class QrModule {}
