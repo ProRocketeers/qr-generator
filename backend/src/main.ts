2e343e699cc2a30952c1c80010ddfc322a2e7e93
Ondra Sýkora <ondrej@sykora.it>
2026-03-21 22:14:17 +0100

    fix: reorder middleware setup for improved application initialization

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,15 +10,16 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   const port = Number(process.env.PORT || 3000)
 
-  // Register global exception filter
+  app.enableCors()
+
   app.useGlobalFilters(new GlobalExceptionFilter())
 
-  configureSwagger(app)
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   )
 
-  app.enableCors()
+  configureSwagger(app)
+
   await app.listen(port)
 }
 
